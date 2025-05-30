732e66ce0432beca364688f3016c12266014570e
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2025-05-31 00:24:55 +0530

    Create the authentication controller

---
+++ book-network/src/main/java/com/hashan/book/auth/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.hashan.book.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(RegistrationRequest request) {
+    }
+}
